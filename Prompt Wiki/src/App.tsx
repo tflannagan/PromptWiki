2b5b2075a8e5730c596e5dba7167b234f681739a
Ty Flannagan <tylerflannagan@yahoo.com>
2023-02-09 19:08:15 -0800

    init

---
+++ Prompt Wiki/src/App.tsx
@@ -0,0 +1,138 @@
+
+import './App.css'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <div>
+      <h1>The Prompt Wiki</h1>
+        
+        <blockquote>
+        <h2>Top (+) Prompts:</h2>
+
+        <div className="module">
+          <p>splash art of a decayed sacrificial altar in overgrown jungle ruins, primitive, primal, mossy, dense jungle canopy overhead, sunlight filtered through leaves, small animals nearby, peaceful mood, dim lighting, action shot, heroic fantasy art, special effects, hd octane render</p>
+            <CopyToClipboard text="splash art of a decayed sacrificial altar in overgrown jungle ruins, primitive, primal, mossy, dense jungle canopy overhead, sunlight filtered through leaves, small animals nearby, peaceful mood, dim lighting, action shot, heroic fantasy art, special effects, hd octane render">
+              <button>Copy to clipboard</button>
+            </CopyToClipboard>
+          </div>
+
+        <div className="module">
+          <p>Concept Art of futuristic soldiers marching across a field, megalithic buildings on fire in the background, dark ground in the foreground,scifi body armor on the soldiers, epic, volumetric lighting, buildings on fire, vehicles on fire, massive spaceships crashing to the ground, ominous, sunset, volumetric clouds, dramatic lighting, concept art, greg rutkowski, james gurney, johannes voss, , cold colors, adventure game, ready to attack</p>
+            <CopyToClipboard text="Concept Art of futuristic soldiers marching across a field, megalithic buildings on fire in the background, dark ground in the foreground,scifi body armor on the soldiers, epic, volumetric lighting, buildings on fire, vehicles on fire, massive spaceships crashing to the ground, ominous, sunset, volumetric clouds, dramatic lighting, concept art, greg rutkowski, james gurney, johannes voss, , cold colors, adventure game, ready to attack">
+              <button>Copy to clipboard</button>
+            </CopyToClipboard>
+          </div>
+
+        <div className="module">
+          <p>Residential home high end futuristic interior, olson kundig::1 Interior Design by Dorothy Draper, maison de verre, axel vervoordt::2 award winning photography of an indoor-outdoor living library space, minimalist modern designs::1 high end indoor/outdoor residential living space, rendered in vray, rendered in octane, rendered in unreal engine, architectural photography, photorealism, featured in dezeen, cristobal palma::2.5 chaparral landscape outside, black surfaces/textures for furnishings in outdoor space::1 –q 2 –ar 4:7</p>
+            <CopyToClipboard text="Residential home high end futuristic interior, olson kundig::1 Interior Design by Dorothy Draper, maison de verre, axel vervoordt::2 award winning photography of an indoor-outdoor living library space, minimalist modern designs::1 high end indoor/outdoor residential living space, rendered in vray, rendered in octane, rendered in unreal engine, architectural photography, photorealism, featured in dezeen, cristobal palma::2.5 chaparral landscape outside, black surfaces/textures for furnishings in outdoor space::1 –q 2 –ar 4:7">
+              <button>Copy to clipboard</button>
+            </CopyToClipboard>
+          </div>
+
+          <div className="module">
+          <p>ultra-sharp photography of a kpop artist from venizia carneval death woman doctor sitting in an superb pose, perspective from under, superb mask + cinematic shot + dynamic composition, incredibly detailed, sharpen, details + superb dress cyberpunk armor dress with superb details + futuristic Seoul in background + evening with light + perfectionism + ultra detailed hair + ultra detailed skin + ultra detailed eye + close mouth + dress in super haute couture + futuredress for woman + high fashion make up + art by Emil Melmoth, Giger, Marcin Nargraba, Rebecca Millen + award winning realism + close up shot + The composition is balanced and dynamic, futuristic Seoul in the background + The image is shot with a Canon EOS 5D Mark IV and Fujifilm Superia X-TRA 400 film.</p>
+            <CopyToClipboard text="ultra-sharp photography of a kpop artist from venizia carneval death woman doctor sitting in an superb pose, perspective from under, superb mask + cinematic shot + dynamic composition, incredibly detailed, sharpen, details + superb dress cyberpunk armor dress with superb details + futuristic Seoul in background + evening with light + perfectionism + ultra detailed hair + ultra detailed skin + ultra detailed eye + close mouth + dress in super haute couture + futuredress for woman + high fashion make up + art by Emil Melmoth, Giger, Marcin Nargraba, Rebecca Millen + award winning realism + close up shot + The composition is balanced and dynamic, futuristic Seoul in the background + The image is shot with a Canon EOS 5D Mark IV and Fujifilm Superia X-TRA 400 film.">
+              <button>Copy to clipboard</button>
+            </CopyToClipboard>
+          </div>
+
+
+          <div className="module">
+          <p>azure, bright, (dragon knight), medieval, warrior, daemon, sapphire, electric, mist, destruction, portrait, hyperrealism, photorealism, steam, particles, heroic, stoic, profile, intense, creature entity</p>
+          <CopyToClipboard text="azure, bright, (dragon knight), medieval, warrior, daemon, sapphire, electric, mist, destruction, portrait, hyperrealism, photorealism, steam, particles, heroic, stoic, profile, intense, creature entity">
+          <button>Copy to clipboard</button>
+          </CopyToClipboard>
+          </div>
+
+          <div className="module">
+          <p>alien creature underwater, (underwater), alien planet, blue, flora, fauna, water, beautiful, amazement, wonder, huge scale, landscape, wild life, ocean planet, ocean, water, islands, otherworldly, massive, incredible, (blue), (alien), abundance, detailed, photorealism, hyperrealism, underwater landmass, floating islands, fish, large underwater mammals, floating (Pandora) worlds, cyberpunk, cyber, foliage, subtropical</p>
+          <CopyToClipboard text="alien creature underwater, (underwater), alien planet, blue, flora, fauna, water, beautiful, amazement, wonder, huge scale, landscape, wild life, ocean planet, ocean, water, islands, otherworldly, massive, incredible, (blue), (alien), abundance, detailed, photorealism, hyperrealism, underwater landmass, floating islands, fish, large underwater mammals, floating (Pandora) worlds, cyberpunk, cyber, foliage, subtropical">
+          <button>Copy to clipboard</button>
+          </CopyToClipboard>
+          </div>
+
+          <div className="module">
+          <p>Titan hero, mythological hero, dichotomy, Dionysus, detailed character, neurological pathways, (realistic), (protagonist), hyperrealism, brain, neurological structure, neurons, computer infrastructure, nodes, neural landscape, beautiful landscape, vast open lands, horizon, sun, sunset, glow, bright, bloom, volumetric lighting, volumetric clouds</p>
+          <CopyToClipboard text="Titan hero, mythological hero, dichotomy, Dionysus, detailed character, neurological pathways, (realistic), (protagonist), hyperrealism, brain, neurological structure, neurons, computer infrastructure, nodes, neural landscape, beautiful landscape, vast open lands, horizon, sun, sunset, glow, bright, bloom, volumetric lighting, volumetric clouds">
+          <button>Copy to clipboard</button>
+          </CopyToClipboard>
+          </div>
+
+          <div className="module">
+          <p>Artist logo, dark symbol, dark, detailed, Nordic symbols, (T), avatar, profile picture, design art, (cover art), intense, mist, volumetric clouds, volumetric lighting, heroic pose, heroism</p>
+          <CopyToClipboard text="Artist logo, dark symbol, dark, detailed, Nordic symbols, (T), avatar, profile picture, design art, (cover art), intense, mist, volumetric clouds, volumetric lighting, heroic pose, heroism">
+          <button>Copy to clipboard</button>
+          </CopyToClipboard>
+          </div>
+
+          <div className="module">
+          <p>(no text), Artist logo, Music represented by light waves, oscillations, protons, big bang, universe, bright, happy, detailed, content feeling, (T), avatar, profile picture, design art, (cover art), intense, mist, high-detail, layers</p>
+          <CopyToClipboard text="(no text), Artist logo, Music represented by light waves, oscillations, protons, big bang, universe, bright, happy, detailed, content feeling, (T), avatar, profile picture, design art, (cover art), intense, mist, high-detail, layers">
+          <button>Copy to clipboard</button>
+          </CopyToClipboard>
+          </div>
+
+          <div className="module">
+          <p>a breathtaking (photoreal image of an beautiful green planet), sky blue transparent atmosphere + cinematic shot + dynamic composition, incredibly detailed, sharpen, details + RAW color + star light + volumetric clouds + accurately detailed terrain + glowing edge of planet + award winning realism + The composition is balanced and dynamic, warm color star in the background, mass effect andromeda</p>
+          <CopyToClipboard text="a breathtaking (photoreal image of an beautiful green planet), sky blue transparent atmosphere + cinematic shot + dynamic composition, incredibly detailed, sharpen, details + RAW color + star light + volumetric clouds + accurately detailed terrain + glowing edge of planet + award winning realism + The composition is balanced and dynamic, warm color star in the background, mass effect andromeda">
+          <button>Copy to clipboard</button>
+          </CopyToClipboard>
+          </div>
+
+          <div className="module">
+          <p>ultra-sharp photography of a (Viking berserker wearing armor) in an superb pose, superb face + cinematic shot + dynamic composition, incredibly detailed, sharpen, details + superb body armor, armor with superb Viking details + ancient Odin temple in background, ground fog, camera blur + evening with foggy light + perfectionism + award winning realism + close up shot + The composition is balanced and dynamic, ancient Odin temple in the background + The image is shot with a Canon EOS 5D Mark IV and Fujifilm Superia X-TRA 400 film, back into time, history</p>
+          <CopyToClipboard text="ultra-sharp photography of a (Viking berserker wearing armor) in an superb pose, superb face + cinematic shot + dynamic composition, incredibly detailed, sharpen, details + superb body armor, armor with superb Viking details + ancient Odin temple in background, ground fog, camera blur + evening with foggy light + perfectionism + award winning realism + close up shot + The composition is balanced and dynamic, ancient Odin temple in the background + The image is shot with a Canon EOS 5D Mark IV and Fujifilm Superia X-TRA 400 film, back into time, history">
+          <button>Copy to clipboard</button>
+          </CopyToClipboard>
+          </div>
+
+        </blockquote>
+        
+        <blockquote>
+        <h2>Top (-) Prompts:</h2>
+        <div className="module">
+          <p className="neg">blurry, out of focus, out of frame, low-res, bad anatomy, ugly, grain, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, futanari, girl with penis, blood, urine, fat, obese, multiple girls, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, jpeg artifacts, signature, watermark, extra fingers, mutated hands, (multiple penises), (disembodied), (poorly drawn hands), (poorly drawn face), (mutation)</p>
+          <CopyToClipboard text="blurry, out of focus, out of frame, low-res, bad anatomy, ugly, grain, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, futanari, girl with penis, blood, urine, fat, obese, multiple girls, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, jpeg artifacts, signature, watermark, extra fingers, mutated hands, (multiple penises), (disembodied), (poorly drawn hands), (poorly drawn face), (mutation)">
+          <button>COPY TO CLIPBOARD</button>
+          </CopyToClipboard>
+        </div>
+
+        <div className="module">
+          <p className="neg">blurry, out of focus, out of frame, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, jpeg artifacts, signature, watermark, extra fingers, mutated hands,(disembodied), (poorly drawn hands), (poorly drawn face), (mutation)</p>
+          <CopyToClipboard text="blurry, out of focus, out of frame, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, jpeg artifacts, signature, watermark, extra fingers, mutated hands,(disembodied), (poorly drawn hands), (poorly drawn face), (mutation)">
+          <button>COPY TO CLIPBOARD</button>
+          </CopyToClipboard>
+        </div>
+
+        <div className="module">
+          <p className="neg">floating ships, floating objects, blurry, out of focus, out of frame, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, jpeg artifacts, signature, watermark, </p>
+          <CopyToClipboard text="floating ships, floating objects, blurry, out of focus, out of frame, lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, jpeg artifacts, signature, watermark, ">
+          <button>COPY TO CLIPBOARD</button>
+          </CopyToClipboard>
+        </div>
+
+        <div className="module">
+          <p className="neg">lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts,signature, watermark, username, blurry, artist name</p>
+          <CopyToClipboard text="lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts,signature, watermark, username, blurry, artist name">
+          <button>COPY TO CLIPBOARD</button>
+          </CopyToClipboard>
+        </div>
+
+
+        </blockquote>
+      </div>
+
+      <footer> 
+        <h3>© 2023 Clay & Jones. All rights reserved.</h3>
+        <h3 className='footer-sm'>Prompts Curated by Alex & Ty Flannagan</h3>
+      </footer>
+    </div>
+  )
+}
+
+export default App
